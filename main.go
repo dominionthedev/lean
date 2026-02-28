33ff2766871e83c558dfd33f965e56c897ef1a4c
Dominionthedeveloper <Dominionthedeveloper@gmail.com>
2026-02-28 23:41:59 +0100

    feat: update dependencies and modify main execution to include version

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
+// Lean – A smart tool for Managing your env files
+
 package main
 
 import "github.com/dominionthedev/lean/cmd/lean"
 
+// use version from /cmd/lean/version.go
+var version string
+
 func main() {
-	lean.Execute()
+	lean.Execute(version)
 }
